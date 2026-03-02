b2223a5d55476724d827d98326da98e13e44b319
Abhi <metromaxabhinav@gmail.com>
2026-03-02 08:50:57 +0530

    remove unused import

--- src/main/java/com/example/event_management_service/event/model/EventSeat.java
+++ src/main/java/com/example/event_management_service/event/model/EventSeat.java
@@ -11,7 +11,6 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
 import jakarta.persistence.Version;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
