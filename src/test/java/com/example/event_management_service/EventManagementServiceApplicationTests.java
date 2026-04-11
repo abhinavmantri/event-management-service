79b885a10ac1fdcd1dbfa53876d1c76afae2fc20
Abhi <metromaxabhinav@gmail.com>
2026-04-11 17:02:16 +0530

    test cases changes

--- src/test/java/com/example/event_management_service/EventManagementServiceApplicationTests.java
+++ src/test/java/com/example/event_management_service/EventManagementServiceApplicationTests.java
@@ -1,10 +1,17 @@
 package com.example.event_management_service;
 
+import com.example.event_management_service.event.messaging.EventPublishedKafkaMessage;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EventManagementServiceApplicationTests {
+	@MockitoBean
+	private KafkaTemplate<String, EventPublishedKafkaMessage> kafkaTemplate;
 
 	@Test
 	void contextLoads() {
